6506d4eda1e44f468c185f0e4af800c098178b82
Shalu <shalu.r@cubiclogics.com>
2022-08-25 17:58:47 +0530

    update navbar

--- js/Headernavbar.js
+++ js/Headernavbar.js
@@ -2,44 +2,44 @@ $(document).ready(function () {
 $(".navbar-nav.ml-auto").html("");
 
 $(".navbar-nav.ml-auto").append("<li class='nav-item'>"+
-              "<a class='nav-link text-dark' href='https://hr365docs.github.io/docs/'>Home</a>"+
+              "<a class='nav-link text-dark homelink' href='https://hr365docs.github.io/docs/'>Home</a>"+
             "</li>");
 
             $(".navbar-nav.ml-auto").append("<li class='nav-item'>"+
-              "<a class='nav-link text-dark' href='../employee-directory/'>Employee Directory</a>"+
+              "<a class='nav-link text-dark employeedirectorylink' href='../employee-directory/'>Employee Directory</a>"+
             "</li>");
 
 
             $(".navbar-nav.ml-auto").append("<li class='nav-item'>"+
-            "<a class='nav-link text-dark' href='../Asset-Management/'>Asset Management</a>"+
+            "<a class='nav-link text-dark assetmanagementlink' href='../Asset-Management/'>Asset Management</a>"+
           "</li>");
 
 
                 $(".navbar-nav.ml-auto").append("<li class='nav-item'>"+
-                " <a class='nav-link text-dark' href='../helpdesk/'>HR365-Helpdesk</a>"+
+                " <a class='nav-link text-dark helpdesklink' href='../helpdesk/'>HR365-Helpdesk</a>"+
             "</li>");
 
 
                 $(".navbar-nav.ml-auto").append("<li class='nav-item'>"+
-                "<a class='nav-link text-dark' href='../time-off-manager/'>Time-Off Manager</a>"+
+                "<a class='nav-link text-dark timeofmanagerlink' href='../time-off-manager/'>Time-Off Manager</a>"+
                 "</li>");
 
 
                 $(".navbar-nav.ml-auto").append("<li class='nav-item'>"+
-                "<a class='nav-link text-dark' href='../timesheets/'>Timesheet</a>"+
+                "<a class='nav-link text-dark timesheetlink' href='../timesheets/'>Timesheet</a>"+
             "</li>");
 
 
             $(".navbar-nav.ml-auto").append("<li class='nav-item'>"+
-            "<a class='nav-link text-dark' href='../employee-on-boarding/'>Employee On-boarding</a>"+
+            "<a class='nav-link text-dark onboardinglink' href='../employee-on-boarding/'>Employee On-boarding</a>"+
             "</li>");
 
             $(".navbar-nav.ml-auto").append("<li class='nav-item'>"+
-            "<a class='nav-link text-dark' href='../Performance-Management/'>Performance Management</a>"+
+            "<a class='nav-link text-dark performancelink' href='../Performance-Management/'>Performance Management</a>"+
             "</li>");
 
             $(".navbar-nav.ml-auto").append("<li class='nav-item'>"+
-            "<a class='nav-link text-dark' href='../contact/'>Contact</a>"+
+            "<a class='nav-link text-dark performancelink' href='../contact/'>Contact</a>"+
             "</li>");
 
            
