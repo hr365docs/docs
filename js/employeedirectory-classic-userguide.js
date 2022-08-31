ba35100fd02de6100525b2cc5ee31c4c079e2404
Shalu <shalu.r@cubiclogics.com>
2022-08-31 15:07:59 +0530

    update

---
+++ js/employeedirectory-classic-userguide.js
@@ -0,0 +1,95 @@
+$(document).ready(function () {
+$(".sidensvbarlink").html("");
+
+$(".sidensvbarlink").append("<li data-nav-id=https://github.com/hr365docs/My-project.git/installation/requirments/"+
+"title=Introduction class='sidelist active introductionlink'>"+
+"<a href='../intoduction'>"+
+    "Introduction"+
+"</a>"+
+"</li>"+
+"<ul class='ml-15 sidenavSub-topics'>"+
+    "<li data-nav-id=https://github.com/hr365docs/My-project.git/installation/requirments/"+
+    "title=gettingstarted class='sielist gettingstartedlink'>"+
+        "<a href='../gettingstarted/'>Getting started</a>"+
+    "</li>"+
+    "<li data-nav-id=https://github.com/hr365docs/My-project.git/user_profile/requirments/"+
+    "title=Profile class='sidelist userprofilelink'>"+
+        "<a href='../profile/'>Managing User Profile</a>"+
+    "</li>"+
+    "<li data-nav-id=https://github.com/hr365docs/My-project.git/installation/requirments/"+
+    "title=User_interface class='sidelist userinterfacelink' >"+
+    "<div class='dropdownicon' >"+
+        "<a class='qwe' href='../userinterface/'>User interface</a>"+
+        "<img src='../userinterface/down-arrow-svgrepo-com.svg'  class='imageIcon' onclick='hideshowuserinterface(event)'>"+
+    "</div>"+
+       "<ul class='ml-15 sidenavSub-topics' id='userinterecenavbar'>"+
+
+            "<li data-nav-id=https://github.com/hr365docs/My-project.git/installation/requirments/"+
+                "title=home_page class='sidelist homelink'>"+
+                "<a href='../userinterface/#_homepage'>Home Page</a>"+
+            "</li>"+
+            "<li data-nav-id=https://github.com/hr365docs/My-project.git/installation/requirments/"+
+            "title=search class='sidelist searchlink'>"+
+                "<a href='../userinterface/#_searchpage'>Search</a>"+
+
+            "</li>"+
+            "<li data-nav-id=https://github.com/hr365docs/My-project.git/installation/requirments/"+
+            "title=views class='sidelist viewslink'>"+
+                "<a href='../userinterface/#_viewpage'>Views</a>"+
+
+            "</li>"+
+
+        "</ul>"+ 
+    "</li>"+
+
+"</ul>"+
+"</li>");
+
+
+
+});
+
+
+//             $(".navbar-nav.ml-auto").append("<li class='nav-item'>"+
+//               "<a class='nav-link text-dark employeedirectorylink' href='../sharepoint-employee-directory/'>Employee Directory"+
+//             "</li>");
+
+
+//             $(".navbar-nav.ml-auto").append("<li class='nav-item'>"+
+//             "<a class='nav-link text-dark assetmanagementlink' href='../Asset-Management/'>Asset Management</a>"+
+//           "</li>");
+
+
+//                 $(".navbar-nav.ml-auto").append("<li class='nav-item'>"+
+//                 " <a class='nav-link text-dark helpdesklink' href='../helpdesk/'>HR365-Helpdesk</a>"+
+//             "</li>");
+
+
+//                 $(".navbar-nav.ml-auto").append("<li class='nav-item'>"+
+//                 "<a class='nav-link text-dark timeofmanagerlink' href='../time-off-manager/'>Time-Off Manager</a>"+
+//                 "</li>");
+
+
+//                 $(".navbar-nav.ml-auto").append("<li class='nav-item'>"+
+//                 "<a class='nav-link text-dark timesheetlink' href='../timesheets/'>Timesheet</a>"+
+//             "</li>");
+
+
+//             $(".navbar-nav.ml-auto").append("<li class='nav-item'>"+
+//             "<a class='nav-link text-dark onboardinglink' href='../employee-on-boarding/'>Employee On-boarding</a>"+
+//             "</li>");
+
+//             $(".navbar-nav.ml-auto").append("<li class='nav-item'>"+
+//             "<a class='nav-link text-dark performancelink' href='../Performance-Management/'>Performance Management</a>"+
+//             "</li>");
+
+//             $(".navbar-nav.ml-auto").append("<li class='nav-item'>"+
+//             "<a class='nav-link text-dark performancelink' href='../contact/'>Contact</a>"+
+//             "</li>");
+
+           
+
+           
+
+
+
