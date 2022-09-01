48b1a033a97d378ea8df460b6dffd05b724e856c
Shalu <shalu.r@cubiclogics.com>
2022-09-01 13:47:49 +0530

    update

--- js/Headernavbar.js
+++ js/Headernavbar.js
@@ -50,23 +50,23 @@
 // $(".navbar-nav.ml-auto").append("<li class='nav-item'>"+
 // "<a class='nav-link text-dark' href='"+ linkref+"/sharepoint-employee-directory/'>Employee Directory"+
 // "</li>");
-$("#userinterecenavbar").hide();  
-var hideshowinterface="hide"
-function hideshowuserinterface(event){
-         event.stopPropagation();
-       event.preventDefault();
+// $("#userinterecenavbar").hide();  
+// var hideshowinterface="show"
+// function hideshowuserinterface(event){
+//          event.stopPropagation();
+//        event.preventDefault();
        
        
-       $(".imageIcon").toggleClass("drodown_icon");
-        // $(".dropdownicon img").css("border", "1px solid black")
-         if(hideshowinterface == "show"){
+//        $(".imageIcon").toggleClass("drodown_icon");
+//         // $(".dropdownicon img").css("border", "1px solid black")
+//          if(hideshowinterface == "show"){
 
-             $("#userinterecenavbar").hide(); 
-             hideshowinterface="hide";
-         }
-         else{
+//              $("#userinterecenavbar").hide(); 
+//              hideshowinterface="hide";
+//          }
+//          else{
 
-             $("#userinterecenavbar").show();
-             hideshowinterface="show";
-         }
-     }
+//              $("#userinterecenavbar").show();
+//              hideshowinterface="show";
+//          }
+//      }
