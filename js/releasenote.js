51b963bef327b841ebb00af173f9b53d5b537686
Shalu <shalu.r@cubiclogics.com>
2022-09-07 16:23:34 +0530

    upadte

--- js/releasenote.js
+++ js/releasenote.js
@@ -3,12 +3,16 @@ let commonscroll = document.querySelectorAll(".releasenotepadding")
     let dropdowminneritem = document.querySelectorAll(".versiononclick");
     dropdowminneritem.forEach((element, index) => {
         element.addEventListener("click", () => {
-            let commonscrollactive = document.querySelector(".releasenotepadding.active")
-            if (commonscrollactive) {
-                commonscrollactive.classList.remove("active")
-                commonscrollactive.removeAttribute("style")
+            for (let i of dropdowminneritem) {
+                i.classList.remove("active")
+                for (let j of commonscroll) {
+                    j.classList.remove("active")
+                    j.removeAttribute("style")
+                }
             }
+            
             commonscroll[index].classList.add("active")
+            element.classList.add("active")
             commonscroll[index].setAttribute("style", "padding-top:80px!important")
         })
     })
