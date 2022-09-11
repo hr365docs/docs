f8f7a6f9854dd55998c28f90552f27d55b06f497
Shalu <shalu.r@cubiclogics.com>
2022-09-11 18:26:19 +0530

    update

---
+++ js/epm-modern-user.js
@@ -0,0 +1,85 @@
+$(document).ready(function () {
+    $("#empmodernuser").html("");
+
+    $("#empmodernuser").append( "<li data-nav-id='./installation/requirments/' title=Introduction class=sidelist>"+
+    "<a href='./introduction/'>"+
+    "Introduction"+
+    "</a>"+  
+"</li>"+
+
+"<li data-nav-id='./installation/requirments/' title= 'Home Page' class=sidelist>"+
+    "<a href='./home/'>"+
+    "Home Page"+
+    "</a>"+  
+"</li>"+
+
+"<li class='dropdownicon sidelist'>"+
+"<a class='qwe' href='./self-review/'>"+
+    "Self Review"+
+    "</a>"+ 
+    "<img src='./down-arrow-svgrepo-com.svg'  class='imageIconmodernuser aside_dropdown_icon'>"+
+"</li>"+
+
+"<li data-nav-id='./installation/requirments/' title='Self Review'class='sidelist dropdown_romove_dot'>"+
+    
+    
+    "<ul class='sidenavSub-topics aside_dropdown_list active'>"+
+        "<li data-nav-id='../General' title= 'Review'  class=sidelist>"+
+          "<a href='./self-review/#review'>"+
+            "Review"+
+            "</a>"+
+            "</li>"+
+
+            "<li data-nav-id='../General' title=' Summary and Plan'  class=sidelist>"+
+                "<a href='./self-review/#summary-and-plan'>"+
+                    "Summary and Plan"+
+                  "</a>"+
+                  "</li>"+
+
+    "</ul>"+
+
+"</li>"+
+
+
+"<li class='dropdownicon sidelist'>"+
+    "<a class='qwe' href='./feedback/'>"+
+        "360 Feedback"+
+     "</a>"+ 
+    "<img src='./down-arrow-svgrepo-com.svg'  class='imageIconmodernuser aside_dropdown_icon'>"+
+"</li>"+
+"<li data-nav-id='./installation/requirments/' title='360 Feedback' class='sidelist dropdown_romove_dot'>"+
+   
+        
+    
+    "<ul class='sidenavSub-topics aside_dropdown_list active'>"+
+            "<li data-nav-id='../General' title='Recieved'  class=sidelist>"+
+            "<a href='./feedback/#received'>"+
+            "Recieved"+
+                "</a>"+
+            "</li>"+
+
+            "<li data-nav-id='../General' title='Given'  class=sidelist>"+
+                "<a href='./feedback/#given'>"+
+                    "Given"+
+                  "</a>"+
+            "</li>"+
+
+            "<li data-nav-id='../General' title='Action'  class=sidelist>"+
+                "<a href='./feedback/#action'>"+
+                    "Action"+
+                  "</a>"+
+            "</li>"+
+
+            "<li data-nav-id='../General' title='Feedback Request'  class=sidelist>"+
+                "<a href='./feedback/#feedback-request'>"+
+                    "Feedback Request"+
+                  "</a>"+
+            "</li>"+
+
+    "</ul>"+
+
+"</li>")
+                                           
+                                        });
+
+                                        
