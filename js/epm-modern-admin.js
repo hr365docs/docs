12ca80fe1c2e3806ea70e77bf414eca6881883ee
Nishu <Nishu.v@cubiclogics.com>
2022-09-14 15:10:49 +0530

    update

--- js/epm-modern-admin.js
+++ js/epm-modern-admin.js
@@ -1,4 +1,143 @@
 $(document).ready(function () {
     $("#epm-modern-admin").html("");
 
-    $("#epm-modern-admin").append(
+    $("#epm-modern-admin").append("<li data-nav-id='./Settings/' title=Introduction class=sidelist>"+
+
+    "<a href='/employee-performance-management/modern/admin/introduction/'>"+
+       " Introduction"+
+   " </a>"+
+"</li>"+
+"<li data-nav-id='./Settings/' title='Home Page' class=sidelist>"+
+
+    "<a href='/employee-performance-management/modern/admin/home/'>"+
+        "Home Page"+
+  "</a>"+
+
+"</li>"+
+
+"<li class='dropdownicon sidelist'>"+
+    "<a  class='qwe' href='/employee-performance-management/modern/admin/administration/'>"+
+       " Administartion"+
+    " </a>"+
+    " <img src='../../down-arrow-svgrepo-com.svg'  class='imageIconmodernuser aside_dropdown_icon'>"+
+"</li>"+
+"<li data-nav-id='./Settings/' title=Administartion class='sidelist dropdown_romove_dot'>"+
+
+    "<ul class='sidenavSub-topics aside_dropdown_list active'>"+
+        
+        "<ul>"+
+           "<li data-nav-id='./Email-Notification/' title=users class='sidelist'>"+
+           "<a href='/employee-performance-management/modern/admin/administration/#'>"+
+          "users"+
+           "</a>"+
+          "</li>"+
+
+           "<li data-nav-id='./Custom-Emails/' title=Categories class='sidelist'>"+
+               "<a href='/employee-performance-management/modern/admin/administration/#'>"+
+              "Categories"+
+            "</a>"+
+          "</li>"+
+
+            "<li data-nav-id='./User/' title=KRA class='sidelist'>"+
+            "<a href='/employee-performance-management/modern/admin/administration/#'>"+
+            "KRA(Key Result Area)"+
+            "</a>"+
+           " </li>"+
+
+            "<li data-nav-id='./General-Settings/' title='Role Template' class='sidelist'>"+
+            "<a href='/employee-performance-management/modern/admin/administration/#'>"+
+           " Role Template"+
+                   " </a>"+
+           " </li>"+
+        "</ul>"+
+   " </ul>"+
+"</li>"+
+
+   
+"<li class='dropdownicon sidelist'>"+
+    "<a  class='qwe' href='/employee-performance-management/modern/admin/settings/'>"+
+        "Settings"+
+    "</a>"+
+    "<img src='../../down-arrow-svgrepo-com.svg'  class='imageIconmainsetting aside_dropdown_icon_setting active'>"+
+"</li>"+
+
+"<li data-nav-id='./Settings/' title=Settings class='sidelist dropdown_romove_dot'>"+
+    "<ul class='sidenavSub-topics aside_dropdown_list_setting active'>"+
+        
+       " <ul>"+
+           
+           " <li data-nav-id='./General-Settings/' title='General Settings' class='sidelist'>"+
+            "<a href='/employee-performance-management/modern/admin/settings/general/#'>"+
+           " General Settings"+
+            "</a>"+
+            "</li>"+
+          
+           " <li data-nav-id='./Custom-Emails/' title=Users class='sidelist'>"+
+                "<a href='/employee-performance-management/modern/admin/settings/users/'>"+
+                "Users"+
+               " </a>"+
+            "</li>"+
+            "<li data-nav-id='./Column-Setting/' title=Notification class='sidelist'>"+
+           "<a href='/employee-performance-management/modern/admin/settings/notification/'>"+
+           " Notification"+
+           "</a>"+
+           "</li>"+
+
+
+           " <li class='dropdownicon sidelist'>"+
+                "<a  class='qwe' href='/employee-performance-management/modern/admin/settings/features/'>"+
+                "Features"+
+                "</a>"+
+                "<img src='../../down-arrow-svgrepo-com.svg'  class='imageIcongeneral aside_dropdown_icon active'>"+
+            "</li>"+
+
+            "<li data-nav-id='./Dashboard-Settings/' title=Features class='sidelist dropdown_romove_dot'>"+
+                
+
+                "<ul class='sidenavSub-topics aside_dropdown_list active'>"+
+                    "<li data-nav-id='./Custom-Emails/' title=Ratings class='sidelist'>"+
+                        "<a href='/employee-performance-management/modern/admin/settings/features/#'>"+
+                       " Ratings"+
+                        "</a>"+
+                    "</li>"+
+
+                    "<li data-nav-id='./Custom-Emails/' title='360 Feedback' class='sidelist'>"+
+                        "<a href='/employee-performance-management/modern/admin/settings/features/#'>"+
+                        "360 Feedback"+
+                        "</a>"+
+                    "</li>"+
+
+                    "<li data-nav-id='./Custom-Emails/' title='Rating Verbatim' class='sidelist'>"+
+                        "<a href='/employee-performance-management/modern/admin/settings/features/#'>"+
+                        "Rating Verbatim"+
+                        "</a>"+
+                    "</li>"+
+
+                    "<li data-nav-id='./Custom-Emails/' title='Appraisal Cycle' class='sidelist'>"+
+                        "<a href='/employee-performance-management/modern/admin/settings/features/#'>"+
+                       " Appraisal Cycle"+
+                       " </a>"+
+                    "</li>"+
+               " </ul>"+
+           " </li>"+
+
+        "</ul>"+
+    "</ul>"+
+
+"</li>"+
+
+"<li data-nav-id='./Settings/' title=Reports class=sidelist>"+
+
+"<a href='/employee-performance-management/modern/admin/reports/'>"+
+" Reports"+
+    "</a>"+
+"</li>"+
+
+"<li data-nav-id='./Settings/' title=Help class=sidelist>"+
+
+    "<a href='/employee-performance-management/modern/admin/help/'>"+
+       " Help"+
+    "</a>"+
+"</li>")
+                                           
+});
