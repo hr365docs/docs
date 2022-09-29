d38315bb3ad5f473693e97e2c919c43c8d01c799
Nishu <Nishu.v@cubiclogics.com>
2022-09-29 15:29:09 +0530

    update

--- sharepoint-helpdesk/classic/user/hdp-user-js/hdp-user-aside.js
+++ sharepoint-helpdesk/classic/user/hdp-user-js/hdp-user-aside.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-    $("#hdp-user-aside").html("<li data-nav-id='./Introduction/' title=Introduction class='sidelist'><a href='/sharepoint-helpdesk/classic/user/introduction/'>Introduction</a></li><li data-nav-id='../getting-started/' title='Getting Started' class='sidelist'><a href='/sharepoint-helpdesk/classic/user/getting-started/'>Getting Started</a></li><li data-nav-id='./Home-Page/' title='Home Page'  class='sidelist'><a href='/sharepoint-helpdesk/classic/user/home-page/'>Home Page</a></li><li data-nav-id='./User-Interface/' title='Help' class='sidelist'><a href='/sharepoint-helpdesk/classic/user/help/'>Help</a></li>")
+    $("#hdp-user-aside").html("");
+    $("#hdp-user-aside").append("<li data-nav-id='./Introduction/' title=Introduction class='sidelist'>"+
+    "<a href='/sharepoint-helpdesk/classic/user/introduction/'>Introduction</a>"+
+    "</li>"+
+    "<li data-nav-id='../getting-started/' title='Getting Started' class='sidelist'>"+
+    "<a href='/sharepoint-helpdesk/classic/user/getting-started/'>Getting Started</a>"+
+    "</li>"+
+    "<li data-nav-id='./Home-Page/' title='Home Page'  class='sidelist'>"+
+    "<a href='/sharepoint-helpdesk/classic/user/home-page/'>Home Page</a>"+
+    "</li>"+
+    "<li data-nav-id='./User-Interface/' title='Help' class='sidelist'>"+
+    "<a href='/sharepoint-helpdesk/classic/user/help/'>Help</a>"+
+    "</li>");
 })
