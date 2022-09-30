f3b2e5ee48d25a6973ebf03e047d809ee277e7df
Nishu <Nishu.v@cubiclogics.com>
2022-09-30 10:04:40 +0530

    update

--- online-asset-management-application/classic/admin/aside-assets/aside-assets.js
+++ online-asset-management-application/classic/admin/aside-assets/aside-assets.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
     
     "<li class='dropdownicon sidelist'>"+
      "<a href='/online-asset-management-application/classic/admin/assets/'> Assets </a>"+
-     "<img  class='assets-img aside_dropdown_icon' src='/online-asset-management-application/classic/user/user-assets/down-arrow-svgrepo-com.svg'>"+
+     "<img  class='imageIconmodernuser aside_dropdown_icon' src='/online-asset-management-application/classic/user/user-assets/down-arrow-svgrepo-com.svg'>"+
      "</li>"+
      "<li data-nav-id='./Settings/' title='Assets'  class='sidelist dropdown_romove_dot'>"+
      "<ul class='sidenavSub-topics aside_dropdown_list'>"+
@@ -42,7 +42,7 @@ $(document).ready(function() {
            
               "<li class='dropdownicon sidelist'>"+
                "<a href='/online-asset-management-application/classic/admin/consumable/'> Consumable </a>"+
-               "<img class='consumable-img aside_dropdown_icon' src='/online-asset-management-application/classic/user/user-assets/down-arrow-svgrepo-com.svg'>"+
+               "<img class='imageIconmodernuser aside_dropdown_icon' src='/online-asset-management-application/classic/user/user-assets/down-arrow-svgrepo-com.svg'>"+
                "</li>"+
 
                "<li data-nav-id='./Settings/' title='Consumable'  class='sidelist dropdown_romove_dot'>"+
@@ -72,7 +72,7 @@ $(document).ready(function() {
                    
                      "<li class='settings-dropdown sidelist'> "+
                      "<a href='/online-asset-management-application/classic/admin/settings/'> Settings </a>"+
-                     "<img class='aside_dropdown_icon' src='/online-asset-management-application/classic/user/user-assets/down-arrow-svgrepo-com.svg'>"+
+                     "<img class='imageIconmodernuser aside_dropdown_icon' src='/online-asset-management-application/classic/user/user-assets/down-arrow-svgrepo-com.svg'>"+
                     "</li>"+
 
                    "<li data-nav-id='./Settings/' title='Settings'  class='sidelist dropdown_romove_dot'>"+
@@ -100,7 +100,7 @@ $(document).ready(function() {
 
                        "<li class='dropdownicon sidelist'>"+
                         "<a href='/online-asset-management-application/classic/admin/administration/'> Administration </a>"+
-                        "<img  class='administr-img aside_dropdown_icon'  src='/online-asset-management-application/classic/user/user-assets/down-arrow-svgrepo-com.svg'>"+
+                        "<img class='imageIconmodernuser aside_dropdown_icon'  src='/online-asset-management-application/classic/user/user-assets/down-arrow-svgrepo-com.svg'>"+
                         "</li>"+
                         "<li data-nav-id='./Settings/' title='Administration' class='sidelist dropdown_romove_dot'>"+
                         "<ul class='sidenavSub-topics aside_dropdown_list'>"+
@@ -152,7 +152,7 @@ $(document).ready(function() {
                             "</li>"+
                              "</ul>"+
                              "</li>"+
-                             
+
                              "<li data-nav-id='./Report/' title='Report' class='sidelist'>"+ 
                              "<a href='/online-asset-management-application/classic/admin/report/'> Report </a>"+
                              "</li>"+
@@ -197,22 +197,21 @@ window.addEventListener("load", () => {
         var koopId = baseUrl.substring(baseUrl.lastIndexOf('#') + 1);
         let getid = document.getElementsByTagName("div");
         let getancher = document.getElementsByTagName("a");
-        let countlenght = getancher.length
-        for (let i = 0; i < countlenght; i++) {
+        for (let i = 0; i < getancher.length; i++) {
             if (getid.length > i) {
                 if (koopId == getid[i].id) {
                     let id = getid[i];
                     id.classList.add("active")
                     id.setAttribute("style", "padding-top:80px!important");
                 }
+
             }
             if (getid[0].baseURI == getancher[i].href) {
                 getancher[i].parentNode.classList.add("active")
-                    // console.log(getid[0].baseURI)
             }
+
+
         }
-
-
 
     }
     getid()
@@ -264,15 +263,92 @@ window.addEventListener("load", () => {
             }
         })
     })
-
-    // Setting open dropdown js code
-    if (document.querySelector(".aside_dropdown_icon_setting")) {
-        let dropdownsetting = document.querySelector(".aside_dropdown_icon_setting");
-        let dropdownitemssetting = document.querySelector(".aside_dropdown_list_setting");
-        dropdownsetting.addEventListener("click", () => {
-            dropdownsetting.classList.toggle("active")
-            dropdownitemssetting.classList.toggle("active")
-        })
-
-    }
 })
+// window.addEventListener("load", () => {
+
+
+//     // new page add padding onclick js code
+//     function getid() {
+//         var baseUrl = (window.location).href;
+//         var koopId = baseUrl.substring(baseUrl.lastIndexOf('#') + 1);
+//         let getid = document.getElementsByTagName("div");
+//         let getancher = document.getElementsByTagName("a");
+//         let countlenght = getancher.length
+//         for (let i = 0; i < countlenght; i++) {
+//             if (getid.length > i) {
+//                 if (koopId == getid[i].id) {
+//                     let id = getid[i];
+//                     id.classList.add("active")
+//                     id.setAttribute("style", "padding-top:80px!important");
+//                 }
+//             }
+//             if (getid[0].baseURI == getancher[i].href) {
+//                 getancher[i].parentNode.classList.add("active")
+//                     // console.log(getid[0].baseURI)
+//             }
+//         }
+
+
+
+//     }
+//     getid()
+
+//     // common onclick scroll js code 
+//     let commonscroll = document.querySelectorAll(".common-scroll")
+//     let dropdowmperent = document.querySelectorAll(".aside_dropdown_list");
+//     for (let i of dropdowmperent) {
+//         let dropdowminneritem = i.querySelectorAll(".aside_dropdown_list li");
+//         dropdowminneritem.forEach((element, index) => {
+//             element.addEventListener("click", () => {
+//                 for (let i of dropdowminneritem) {
+//                     i.classList.remove("active")
+//                     for (let j of commonscroll) {
+//                         j.classList.remove("active")
+//                         j.removeAttribute("style")
+//                     }
+//                 }
+//                 element.classList.add("active")
+//                 commonscroll[index].classList.add("active")
+//                 commonscroll[index].setAttribute("style", "padding-top:80px!important")
+//             })
+//         })
+//     }
+
+
+//     // Dropdown js Code
+//     let dropdowmitems = document.querySelectorAll(".aside_dropdown_icon");
+//     let dropdowmlist = document.querySelectorAll(".aside_dropdown_list");
+
+//     dropdowmitems.forEach((element, index) => {
+//         element.addEventListener("click", () => {
+//             let getheight = dropdowmlist[index].scrollHeight;
+//             let dropdowmlistactive = document.querySelector(".aside_dropdown_list.active")
+//             if (element.classList.contains("active")) {
+//                 element.classList.remove("active");
+//                 dropdowmlistactive.setAttribute("style", "height:0px;overflow:hidden;opacity:0;")
+//             } else {
+//                 for (let i of dropdowmitems) {
+//                     i.classList.remove("active");
+//                 }
+//                 for (let j of dropdowmlist) {
+//                     j.classList.remove("active");
+//                     j.setAttribute("style", "height:0px;overflow:hidden;opacity:0;")
+//                 }
+//                 element.classList.add("active");
+//                 dropdowmlist[index].classList.add("active")
+//                 dropdowmlist[index].setAttribute("style", `height:${getheight}px;opacity: 1;`)
+//             }
+//         })
+//     })
+
+//     // Setting open dropdown js code
+//     if (document.querySelector(".aside_dropdown_icon_setting")) {
+//         let dropdownsetting = document.querySelector(".aside_dropdown_icon_setting");
+//         let dropdownitemssetting = document.querySelector(".aside_dropdown_list_setting");
+//         dropdownsetting.addEventListener("click", () => {
+//             dropdownsetting.classList.toggle("active")
+//             dropdownitemssetting.classList.toggle("active")
+//         })
+
+//     }
+// })
