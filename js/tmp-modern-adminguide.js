59579418d3e15fd1d6bfdf39403e9d6985a94794
Nishu <Nishu.v@cubiclogics.com>
2023-01-19 16:54:59 +0530

    update

---
+++ js/tmp-modern-adminguide.js
@@ -0,0 +1,86 @@
+$(document).ready(function () {
+    $("#tmpmodernadmin").html("");
+
+    $("#tmpmodernadmin").append("<ul>"+
+        "<li data-nav-id='../Introduction' title=Introduction class='sidelist'>"+
+        "<a href='/employee-vacation-tracker-time-off-manager/modern/admin/introduction/'>"+
+        "Introduction"+
+        "</a>"+
+        "</li>"+
+    
+        "<li data-nav-id='../Home-page' title=Home-page class=sidelist>"+
+            "<a href='/employee-vacation-tracker-time-off-manager/modern/admin/home-page/'>"+
+                "Home Page"+
+            "</a>"+
+            "</li>"+
+        
+        "<li data-nav-id='../ HR-actions' title= HR-actions class=sidelist>"+
+        "<a href='/employee-vacation-tracker-time-off-manager/modern/admin/hr-actions/'>"+
+           "HR actions"+
+        "</a>"+
+        "</li>"+
+    
+        "<li data-nav-id='../holidays' title=holidays class='sidelist'>"+
+                "<a href='/employee-vacation-tracker-time-off-manager/modern/admin/holidays/'>"+
+                    "Public Holidays"+
+                "</a>"+
+                "</li>"+
+    
+        "<li data-nav-id='../user' title=user class='sidelist'>"+
+                    "<a href='/employee-vacation-tracker-time-off-manager/modern/admin/user/'>"+
+                    "Users"+
+                    "</a>"+
+                    "</li>"+
+    
+        "<li data-nav-id='../user' title=user class='sidelist'>"+
+                        "<a href='/employee-vacation-tracker-time-off-manager/modern/admin/reports/'>"+
+                        "Reports"+
+                        "</a>"+
+                        "</li>"+
+         
+
+                        "<li class='dropdownicon sidelist'>"+
+                        "<a class='qwe' href='/employee-vacation-tracker-time-off-manager/modern/admin/settings/'>"+
+                        "System Settings"+
+                        "</a>"+
+                        "<img src='./down-arrow-svgrepo-com.svg'  class='imageIconmodernuser aside_dropdown_icon'>"+
+                    "</li>"+
+
+
+        "<li data-nav-id='../system-settings' title=system-settings class='sidelist dropdown_romove_dot'>"+
+                        // "<div class='dropdownicon'>"+   
+                        //     "<a class='qwe' href='/employee-vacation-tracker-time-off-manager/modern/admin/settings/'>"+
+                        //     "System Settings"+
+                        //     "</a>"+
+                        //     "<img src='./down-arrow-svgrepo-com.svg'  class='imageIconmodernuser aside_dropdown_icon'>"+
+                        //     "</div>"+
+                            "<ul class='sidenavSub-topics aside_dropdown_list'>"+
+                                "<li data-nav-id='../user' title=user class='sidelist'>"+
+                                    "<a href='/employee-vacation-tracker-time-off-manager/modern/admin/settings/#organization'>"+
+                                    "Organizations"+
+                                    "</a>"+
+                                    "</li>"+
+                                    "<li data-nav-id='../user' title=user class='sidelist'>"+
+                                        "<a href='/employee-vacation-tracker-time-off-manager/modern/admin/settings/#leave-type-settings'>"+
+                                        "Leave Settings"+
+                                        "</a>"+
+                                        "</li>"+
+                                        "<li data-nav-id='../user' title=user class='sidelist'>"+
+                                            "<a href='/employee-vacation-tracker-time-off-manager/modern/admin/settings/#advance-settings'>"+
+                                            "Advance Settings"+
+                                            "</a>"+
+                                            "</li>"+
+                            "</ul>"+
+                            "</li >"+
+    
+                            "<li data-nav-id='../user' title=user class='sidelist'>"+
+                                "<a href='/employee-vacation-tracker-time-off-manager/modern/admin/help/'>"+
+                               "Help"+
+                                "</a>"+
+              
+                            "</li>"+
+        "</ul>")
+                                           
+                                        });
+
+                                        
---
+++ js/tmp-modern-userguide.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    $("#tmpmodernuserguide").html("");
+
+    $("#tmpmodernuserguide").append( "<ul>"+
+        "<li data-nav-id='../Introduction' title='Introduction' class='sidelist'>"+
+        "<a href='/employee-vacation-tracker-time-off-manager/modern/user/introduction/'>"+
+        "Introduction"+
+        "</a>"+
+        "</li>"+
+        "<li data-nav-id='../Introduction' title=Getting-Started class='sidelist'>"+
+            "<a href='/employee-vacation-tracker-time-off-manager/modern/user/getting-started/'>"+
+                "Getting Started"+
+            "</a>"+
+            "</li>"+
+    
+        "<li data-nav-id='../Home-page' title=Home-page class=sidelist>"+
+            "<a href='/employee-vacation-tracker-time-off-manager/modern/user/home-page/'>"+
+                "Home Page"+
+            "</a>"+
+            "</li>"+
+        
+        "<li data-nav-id='../ HR-actions' title= HR-actions class=sidelist>"+
+        "<a href='/employee-vacation-tracker-time-off-manager/modern/user/user-interface/'>"+
+          "User Interface"+
+        "</a>"+
+        "</li>"+
+    
+         "<li data-nav-id='../user' title=user class='sidelist'>"+
+                "<a href='/employee-vacation-tracker-time-off-manager/modern/user/help/'>"+
+                "Help"+
+                "</a>"+
+            "</li>"+
+        "</ul>")
+                                           
+                                        });
+
+                                        
