ab6dedee6470df6853bf77e69fa8c0d2cc34ba9e
Nishu <Nishu.v@cubiclogics.com>
2023-02-03 17:11:35 +0530

    update

--- sharepoint-employee-onboarding/modern/user/js-modern-user/obm-modern-user.js
+++ sharepoint-employee-onboarding/modern/user/js-modern-user/obm-modern-user.js
@@ -5,20 +5,20 @@ $(document).ready(function() {
         " <li data-nav-id='./installation/requirments/' title=Introduction class=sidelist>"+
          "<a href='/sharepoint-employee-onboarding/modern/user/introduction/'> Introduction </a>"+
          "</li>"+
-         "<li data-nav-id=https://github.com/hr365docs/My-project.git/installation/requirments/ title=Getting Started class=sidelist>"+
+         "<li data-nav-id=https://github.com/hr365docs/My-project.git/installation/requirments/ title='Getting Started' class=sidelist>"+
          "<a href='/sharepoint-employee-onboarding/modern/user/getting-started/'>Getting Started</a>"+
-         "<li data-nav-id='./installation/requirments/' title=Home_Page class=sidelist>"+
+         "<li data-nav-id='./installation/requirments/' title=Home Page class=sidelist>"+
          "<a href='/sharepoint-employee-onboarding/modern/user/home-page/'> Home Page </a>"+
          "</li>"+
          
      
-         "<li class='dropdownicon sidelist'>"+
+         "<li class='dropdownicon sidelist'  title='User Interface'>"+
          "<a class='qwe'  href='/sharepoint-employee-onboarding/modern/user/user-interface/'> User Interface </a>"+
          "<img class='helpdesk-img aside_dropdown_icon' src='/sharepoint-employee-onboarding/classic/user/js-user/down-arrow-svgrepo-com.svg'>"+
          "</li>"+
-         "<li data-nav-id='./installation/requirments/' title='User Interface' class='sidelist dropdown_romove_dot'>"+
+         "<li data-nav-id='./installation/requirments/' class='sidelist dropdown_romove_dot'>"+
          "<ul class='sidenavSub-topics aside_dropdown_list'>"+
-          "<li data-nav-id='../General' title='updated Profiles' class='sidelist'>"+
+          "<li data-nav-id='../General' title='Updated Profiles' class='sidelist'>"+
           "<a href='/sharepoint-employee-onboarding/modern/user/user-interface/#update-profiles'> Update Profiles </a>"+
            "</li>"+
            "<li data-nav-id='../General' title=' Update Details' class=sidelist>"+
