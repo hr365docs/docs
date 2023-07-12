3faa302f7009ae0a048cd8bd12546a906460e411
Vishal B <vishal.b@cubiclogics.com>
2023-07-12 10:05:58 +0530

    TS365

--- online-timesheet-software/modern/user/hdm-user-js/hdm-user-aside.js
+++ online-timesheet-software/modern/user/hdm-user-js/hdm-user-aside.js
@@ -2,15 +2,15 @@ $(document).ready(function() {
     $("#hdm-user-aside").html("");
     $("#hdm-user-aside").append(
     "<li data-nav-id='./Introduction/' title=Introduction class='sidelist'>"+
-    "<a href='/sharepoint-helpdesk/modern/user/introduction/'>Introduction</a>"+
+    "<a href='/online-timesheet-software/modern/user/introduction/'>Introduction</a>"+
     "</li>"+
     "<li data-nav-id='../getting-started/' title='Getting Started' class='sidelist'>"+
-    "<a href='/sharepoint-helpdesk/modern/user/getting-started/'>Getting Started</a>"+
+    "<a href='/online-timesheet-software/modern/user/getting-started/'>Getting Started</a>"+
     "</li>"+
     "<li data-nav-id='./Home-Page/' title='Home Page'  class='sidelist'>"+
-    "<a href='/sharepoint-helpdesk/modern/user/home-page/'>Home Page</a>"+
+    "<a href='/online-timesheet-software/modern/user/home-page/'>Home Page</a>"+
     "</li>"+
     "<li data-nav-id='./User-Interface/' title='Help' class='sidelist'>"+
-    "<a href='/sharepoint-helpdesk/modern/user/help/'>Help</a>"+
+    "<a href='/online-timesheet-software/modern/user/help/'>Help</a>"+
     "</li>");
 })
