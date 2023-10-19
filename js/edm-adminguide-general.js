eda8090a3d602d8ca9c9712cf2e67ed9a0168960
hr365docs <97627396+hr365docs@users.noreply.github.com>
2023-10-19 18:31:25 +0530

    abc

--- js/edm-adminguide-general.js
+++ js/edm-adminguide-general.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
   "</li>"+
 
   "<li  data-nav-id='../advanced/' title='Allow users to export directory' class='sidelist'>"+
-    "<a href='/sharepoint-employee-directory/modern/admin/settings/general/#allow-user-export-directory'>Allow users to export directory</a>"+
+    "<a href='/sharepoint-employee-directory/modern/admin/settings/general/#allow-user-export-directory'>Show or Hide modules</a>"+
   "</li>"+
 
   "<li  data-nav-id='../advanced/' title='Language selection' class='sidelist'>"+
@@ -18,26 +18,49 @@ $(document).ready(function () {
   "</li>"+
 
   "<li  data-nav-id='../advanced/' title=' Update users role and permissions' class='sidelist'>"+
-    "<a href='/sharepoint-employee-directory/modern/admin/settings/general/#update-roles'>Update users role and permissions</a>"+
+    "<a href='/sharepoint-employee-directory/modern/admin/settings/general/#update-roles'>Hide Sharepoint Page's Default Components</a>"+
   "</li>"+
 
   "<li  data-nav-id='../advanced/' title=' Update users department' class='sidelist '>"+
-    "<a href='/sharepoint-employee-directory/modern/admin/settings/general/#update-department'>Update users department"+
+    "<a href='/sharepoint-employee-directory/modern/admin/settings/general/#update-department'>Role and permissions"+
     "</a>"+
   "</li>"+
   "<li  data-nav-id='../advanced/' title=' Update job title ' class='sidelist'>"+
-    "<a href='/sharepoint-employee-directory/modern/admin/settings/general/#update-jobtitle'>Update job title"+
+    "<a href='/sharepoint-employee-directory/modern/admin/settings/general/#Filterattribute'>Filter Attributes"+
     "</a>"+
   "</li>"+
   "<li  data-nav-id='../advanced/' title=' Update users location  ' class='sidelist'>"+
-    "<a href='/sharepoint-employee-directory/modern/admin/settings/general/#update-location'>Update users location  </a>"+
+    "<a href='/sharepoint-employee-directory/modern/admin/settings/general/#Map-external-list'> Map external list  </a>"+
   "</li>"+
   "<li  data-nav-id='../advanced/' title='Collaboration ' class='sidelist '>"+
-    "<a href='/sharepoint-employee-directory/modern/admin/settings/general/#collaboration'>Collaboration </a>"+
+    "<a href='/sharepoint-employee-directory/modern/admin/settings/general/#AzureStorageConfiguration'>Azure Storage Configuration </a>"+
   "</li>"+
   "<li  data-nav-id='../advanced/' title='Custom link ' class='sidelist'>"+
-    "<a href='/sharepoint-employee-directory/modern/admin/settings/general/#custom-link'>Custom link</a>"+
-  "</li>"
+    "<a href='/sharepoint-employee-directory/modern/admin/settings/general/#Syncemployeeprofileimagesfrom'>Sync employee profile images from</a>"+
+  "</li>"+
+  "<li  data-nav-id='../advanced/' title='Custom link ' class='sidelist'>"+
+  "<a href='/sharepoint-employee-directory/modern/admin/settings/general/#Recordstoload'>Records to load </a>"+
+"</li>"+
+"<li  data-nav-id='../advanced/' title='Custom link ' class='sidelist'>"+
+"<a href='/sharepoint-employee-directory/modern/admin/settings/general/#Brandlogo'>Brand logo </a>"+
+"</li>"+
+"<li  data-nav-id='../advanced/' title='Custom link ' class='sidelist'>"+
+"<a href='/sharepoint-employee-directory/modern/admin/settings/general/#Birthdayandanniversarytemplates'>Birthday and anniversary templates </a>"+
+"</li>"+"<li  data-nav-id='../advanced/' title='Custom link ' class='sidelist'>"+
+"<a href='/sharepoint-employee-directory/modern/admin/settings/general/#GCCTenant'>GCC Tenant</a>"+
+"</li>"+
+"<li  data-nav-id='../advanced/' title='Custom link ' class='sidelist'>"+
+"<a href='/sharepoint-employee-directory/modern/admin/settings/general/#Employeeinfopage'>Employee info page</a>"+
+"</li>"+
+"<li  data-nav-id='../advanced/' title='Custom link ' class='sidelist'>"+
+"<a href='/sharepoint-employee-directory/modern/admin/settings/general/#Profilecardcustomurlicon'>Profile card custom url icon</a>"+
+"</li>"+
+"<li  data-nav-id='../advanced/' title='Custom link ' class='sidelist'>"+
+"<a href='/sharepoint-employee-directory/modern/admin/settings/general/#Homepagecustomurl'>Home page custom url</a>"+
+"</li>"
+
+
+  
  )
                                            
                                         });
