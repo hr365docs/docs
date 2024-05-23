4359374f69d19797a32277e161c4b4cdda7ba864
Ritika Gilhotra <ritika.g@cubiclogics.com>
2024-05-23 13:10:21 +0530

    clm

--- js/clm-modern-admin.js
+++ js/clm-modern-admin.js
@@ -21,21 +21,26 @@ $(document).ready(function () {
   "</a>"+
 
 "</li>"+
-// "<li data-nav-id='./Settings/' title='Dashboard' class=sidelist>"+
+"<li class='dropdownicon sidelist' title='Help'>" +
+"<a href='/contract-management/modern/admin/dashboard/'>" +
+"Dashboard" +
+"</a>" +
+"<img src='../../down-arrow-svgrepo-com.svg' class='imageIconadvanced aside_dropdown_icon' '>" +
+"</li>" +
 
-//     "<a href='/contract-management/modern/admin/dashboard/'>"+
-//         "Dashboard"+
-//   "</a>"+
+"<li data-nav-id='../advanced/' title=Help class='sidelist dropdown_romove_dot'>" +
 
-// "</li>"+
-// "</li>"+
-// "<li data-nav-id='./Settings/' title='Contracts' class=sidelist>"+
 
-//     "<a href='/contract-management/modern/admin/contracts/'>"+
-//         "Contracts"+
-//   "</a>"+
 
-// "</li>"+
+"<ul class='sidenavSub-topics aside_dropdown_list' id='modernsettingedm'>" +
+"<li  data-nav-id='../advanced/' title='Configure SharePoint API permissions' class='sidelist'>" +
+"<a href='/contract-management/modern/admin/dashboard/#Contract'>Contract</a>" +
+"</li>"+
+"<li  data-nav-id='../advanced/' title='Configure SharePoint API permissions' class='sidelist'>" +
+"<a href='/contract-management/modern/admin/dashboard/#Request'>Request</a>" +
+"</li>"+
+"</ul>"+
+
 "<li data-nav-id='./Settings/' title=Reports class=sidelist>"+
 
 "<a href='/contract-management/modern/admin/reports/'>"+
@@ -43,121 +48,7 @@ $(document).ready(function () {
     "</a>"+
 "</li>"+
 
-// "<li class='dropdownicon sidelist' title='Roles'>"+
-//     "<a  class='qwe' href='/contract-management/modern/admin/roles/'>"+
-//        " Roles"+
-//     " </a>"+
-//     " <img src='../../down-arrow-svgrepo-com.svg'  class='imageIconmodernuser aside_dropdown_icon'>"+
-// "</li>"+
-// "<li data-nav-id='./Settings/'  class='sidelist dropdown_romove_dot'>"+
-// "<ul class='sidenavSub-topics aside_dropdown_list active'>"+
-// "<ul>"+
-  
-   
-//         "<li data-nav-id='./Email-Notification/' title=users class='sidelist'>"+
-//        "<a href='/contract-management/modern/admin/roles/#user'>User</a>"+
-//         "</li>"+
 
-//         "<li data-nav-id='./Custom-Emails/' title=Manager class='sidelist'>"+
-//            " <a href='/contract-management/modern/admin/roles/#manager'>Manager(Employee's Manger)</a>"+
-//        " </li>"+
-
-//        " <li data-nav-id='./User/' title='HR Executive' class='sidelist'>"+
-//        "<a href='/contract-management/modern/admin/roles/#hr-executive-dashboard'>HR Executive-Dashboard</a>"+
-//         "</li>"+
-
-//        "<li data-nav-id='./General-Settings/' title='HR Manager' class='sidelist'>"+
-//             "<a href='/contract-management/modern/admin/roles/#hr-manager-dashboard'>HR Manager-Dashboard </a>"+
-//         "</li>"+
-//         "<li data-nav-id='./General-Settings/' title='HR Head' class='sidelist'>"+
-//         "<a href='/contract-management/modern/admin/roles/#hr-head-dashboard'>HR Head-Dashboard </a>"+
-//    " </li>"+
-//     "<li data-nav-id='./General-Settings/' title='HR Manager Admin' class='sidelist'>"+
-//     "<a href='/contract-management/modern/admin/roles/#hr-manager-admin-dashboard'>HR Manager Admin-Dashboard </a>"+
-// "</li>"+
-// "<li data-nav-id='./General-Settings/' title='HR Head Admin' class='sidelist'>"+
-// "<a href='/contract-management/modern/admin/roles/#hr-head-admin'>HR Head–Admin </a>"+
-// "</li>"+
-//     "</ul>"+
-// "</ul>"+
-// "</li>"+
-
-// "<li data-nav-id='./Settings/'  class='sidelist dropdown_romove_dot'>"+
-
-//     "<ul class='sidenavSub-topics aside_dropdown_list active'>"+
-        
-//         "<ul>"+
-//            "<li data-nav-id='./Email-Notification/' title=Approval Workflow class='sidelist'>"+
-//            "<a href='/contract-management/modern/admin/administration/#approval-workflow'>"+
-//           "Approval Workflow"+
-//            "</a>"+
-//           "</li>"+
-
-//            "<li data-nav-id='./Custom-Emails/' title=Users class='sidelist'>"+
-//                "<a href='/contract-management/modern/admin/administration/#users'>"+
-//               "Users"+
-//             "</a>"+
-//           "</li>"+
-
-//             "<li data-nav-id='./User/' title=Departments class='sidelist'>"+
-//             "<a href='/contract-management/modern/admin/administration/#departments'>"+
-//             "Departments"+
-//             "</a>"+
-//            " </li>"+
-
-//             "<li data-nav-id='./General-Settings/' title='Job Title' class='sidelist'>"+
-//             "<a href='/contract-management/modern/admin/administration/#job-title'>"+
-//            " Job Title"+
-//                    " </a>"+
-//            " </li>"+
-//            "<li data-nav-id='./General-Settings/' title='locations' class='sidelist'>"+
-//            "<a href='/contract-management/modern/admin/administration/#locations'>"+
-//           " Locations"+
-//                   " </a>"+
-//           " </li>"+
-//           "<li data-nav-id='./General-Settings/' title='role' class='sidelist'>"+
-//           "<a href='/contract-management/modern/admin/administration/#role'>"+
-//          " Role"+
-//                  " </a>"+
-//          " </li>"+
-//          "<li data-nav-id='./General-Settings/' title='Business Unit' class='sidelist'>"+
-//          "<a href='/contract-management/modern/admin/administration/#business-unit'>"+
-//         " Business Unit"+
-//                 " </a>"+
-//         " </li>"+
-//         "<li data-nav-id='./General-Settings/' title='Clause Type' class='sidelist'>"+
-//         "<a href='/contract-management/modern/admin/administration/#clause-type'>"+
-//        " Clause Type"+
-//                " </a>"+
-//        " </li>"+
-//        "<li data-nav-id='./General-Settings/' title='Contract Categories' class='sidelist'>"+
-//         "<a href='/contract-management/modern/admin/administration/#contract-categories'>"+
-//        "Contract Categories"+
-//                " </a>"+
-//        " </li>"+
-//        "<li data-nav-id='./General-Settings/' title='Counterparty Types' class='sidelist'>"+
-//        "<a href='/contract-management/modern/admin/administration/#counterparty-types'>"+
-//       "Counterparty Types"+
-//               " </a>"+
-//       " </li>"+
-//       "<li data-nav-id='./General-Settings/' title='Obligation Categories' class='sidelist'>"+
-//       "<a href='/contract-management/modern/admin/administration/#obligation-categories'>"+
-//      "Obligation Categories"+
-//              " </a>"+
-//      " </li>"+
-//      "<li data-nav-id='./General-Settings/' title='Termination Reasons' class='sidelist'>"+
-//      "<a href='/contract-management/modern/admin/administration/#termination-reasons'>"+
-//     "Termination Reasons"+
-//             " </a>"+
-//     " </li>"+
-//     "<li data-nav-id='./General-Settings/' title='Clause Library' class='sidelist'>"+
-//     "<a href='/contract-management/modern/admin/administration/#clause-library'>"+
-//    "Clause Library"+
-//            " </a>"+
-//    " </li>"+
-//         "</ul>"+
-//    " </ul>"+
-// "</li>"+
 
 "<li data-nav-id='./Settings/' title=Reports class=sidelist>"+
 
