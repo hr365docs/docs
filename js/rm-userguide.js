33a47ca3b3393de7261059fff3dbd6f35f151d69
Ritika Gilhotra <ritika.g@cubiclogics.com>
2024-05-28 14:42:32 +0530

    rm365

---
+++ js/rm-userguide.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    $("#rmmmodernuser").html("");
+
+    $("#rmmmodernuser").append( "<li data-nav-id='./installation/requirments/' title=Introduction class=sidelist>"+
+    "<a href='/rm365/modern/user/introduction/'>"+
+    "Introduction"+
+    "</a>"+  
+"</li>"+
+
+"<li data-nav-id='./installation/requirments/' title= 'Home Page' class=sidelist>"+
+    "<a href='/rm365/modern/user/home/'>"+
+    "Home Page"+
+    "</a>"+  
+"</li>"+
+"<li class='dropdownicon sidelist' title='Draft'>"+
+    "<a class='qwe' href='/rm365/modern/user/draft/'>"+
+        "Draft"+
+     "</a>"+ 
+  
+"</li>"+
+
+
+"<li class='dropdownicon sidelist' title='Help'>"+
+    "<a class='qwe' href='/rm365/modern/user/help/'>"+
+        "Help"+
+     "</a>"+ 
+  
+"</li>"
+
+)
+                                           
+                                        });
+
+                                        
