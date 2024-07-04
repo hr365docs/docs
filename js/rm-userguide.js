4fd6084663798b4a6aa7d9419633a86153d5e665
Ritika Gilhotra <ritika.g@cubiclogics.com>
2024-07-04 10:43:17 +0530

    hg

--- js/rm-userguide.js
+++ js/rm-userguide.js
@@ -2,18 +2,18 @@ $(document).ready(function () {
     $("#rmmmodernuser").html("");
 
     $("#rmmmodernuser").append( "<li data-nav-id='./installation/requirments/' title=Introduction class=sidelist>"+
-    "<a href='/rm365/modern/user/introduction/'>"+
+    "<a href='/RM365/modern/user/introduction/'>"+
     "Introduction"+
     "</a>"+  
 "</li>"+
 
 "<li data-nav-id='./installation/requirments/' title= 'Home Page' class=sidelist>"+
-    "<a href='/rm365/modern/user/home/'>"+
+    "<a href='/RM365/modern/user/home/'>"+
     "Home Page"+
     "</a>"+  
 "</li>"+
 "<li class='dropdownicon sidelist' title='Draft'>"+
-    "<a class='qwe' href='/rm365/modern/user/draft/'>"+
+    "<a class='qwe' href='/RM365/modern/user/draft/'>"+
         "Draft"+
      "</a>"+ 
   
@@ -21,7 +21,7 @@ $(document).ready(function () {
 
 
 "<li class='dropdownicon sidelist' title='Help'>"+
-    "<a class='qwe' href='/rm365/modern/user/help/'>"+
+    "<a class='qwe' href='/RM365/modern/user/help/'>"+
         "Help"+
      "</a>"+ 
   
