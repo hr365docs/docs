b1e88f52c32bb034c4c898e375e292dc88096f09
Sukhdev Singh <i-sukhdev.S@cubiclogics.com>
2025-01-08 15:38:06 +0530

    gettingstartingOptionRemoveForUserGuide

--- online-timesheet-software/modern/user/hdm-user-js/hdm-user-aside.js
+++ online-timesheet-software/modern/user/hdm-user-js/hdm-user-aside.js
@@ -4,9 +4,9 @@ $(document).ready(function() {
     "<li data-nav-id='./Introduction/' title=Introduction class='sidelist'>"+
     "<a href='/online-timesheet-software/modern/user/introduction/'>Introduction</a>"+
     "</li>"+
-    "<li data-nav-id='../getting-started/' title='Getting Started' class='sidelist'>"+
-    "<a href='/online-timesheet-software/modern/user/getting-started/'>Getting Started</a>"+
-    "</li>"+
+    // "<li data-nav-id='../getting-started/' title='Getting Started' class='sidelist'>"+
+    // "<a href='/online-timesheet-software/modern/user/getting-started/'>Getting Started</a>"+
+    // "</li>"+
     "<li data-nav-id='./Home-Page/' title='Home Page'  class='sidelist'>"+
     "<a href='/online-timesheet-software/modern/user/home-page/'>Home Page</a>"+
     "</li>"+
