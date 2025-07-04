2a740a91c756cb16da46d36fc461ef1da5bdce4c
Vidya <poojarividya690@gmail.com>
2025-07-04 12:18:49 +0530

    Revenue365

---
+++ js/rev-modern-user.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    $("#revuser").html("");
+
+    $("#revuser").append(
+        "<li data-nav-id='./user/' title='Customer Journey' class='sidelist'>" +
+        "<a href='/Revenue365/modern/user/Introduction/'>Introduction</a>" +
+        "</li>" +
+        "<li data-nav-id='./user/' title='Home Page' class='sidelist'>" +
+        "<a href='/Revenue365/modern/user/homepage/'>Home Page</a>" +
+        "</li>" +
+
+        "<li data-nav-id='./user/' title='Buyers' class='sidelist'>" +
+        "<a href='/Revenue365/modern/user/buyers/'>Buyers</a>" +
+        "</li>" +
+
+        "<li data-nav-id='./user/' title='Dashboard' class='sidelist'>" +
+        "<a href='/Revenue365/modern/user/dashboard/'>Dashboard</a>" +
+        "</li>" +
+
+        "<li data-nav-id='./user/' title='Reports' class='sidelist'>" +
+        "<a href='/Revenue365/modern/user/reports/'>Reports</a>" +
+        "</li>" +
+
+        "<li data-nav-id='./user/' title='Administration' class='sidelist'>" +
+        "<a href='/Revenue365/modern/user/administration/'>Administration</a>" +
+        "</li>" +
+
+        "<li data-nav-id='./user/' title='Catalog' class='sidelist'>" +
+        "<a href='/Revenue365/modern/user/catalog/'>Catalog</a>" +
+        "</li>" +
+
+        // "<li data-nav-id='./user/' title='Customer Journey' class='sidelist'>" +
+        // "<a href='/Revenue365/modern/user/costomerjourney/'>Customer Journey</a>" +
+        // "</li>" +
+
+        "<li data-nav-id='./user/' title='Help' class='sidelist'>" +
+        "<a href='/Revenue365/modern/user/help/'>Help</a>" +
+        "</li>"
+    );
+});
