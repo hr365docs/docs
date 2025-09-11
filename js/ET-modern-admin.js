dcad4dfc925e2e902571360033cc5b6b28e3d26b
Vidya200230 <poojarividya690@gmail.com>
2025-09-11 14:27:07 +0530

    kb.hr365

--- js/ET-modern-admin.js
+++ js/ET-modern-admin.js
@@ -50,102 +50,116 @@ $(document).ready(function () {
         "<a class='qwe' href='/Expense-tracker/modern/admin/settings/'>" +
         "Settings" +
         "</a>" +
-        "<img src='../../down-arrow-svgrepo-com.svg'  class='helpdesk-img aside_dropdown_icon_setting'>" +
+        "<img src='/sharepoint-employee-onboarding/modern/admin/js-modern-admin/down-arrow-svgrepo-com.svg'  class='helpdesk-img aside_dropdown_icon_setting'>" +
         "</li>" +
 
 
         "<li data-nav-id='../system-settings' title=system-settings class='sidelist dropdown_romove_dot'>" +
         "<ul class='sidenavSub-topics aside_dropdown_list_setting'>" +
         "<li data-nav-id='../user' title=General class='dropdownicon sidelist'>" +
-        "<a class='qwe' href='/Expense-tracker/modern/admin/settings/#General-setting'> General Settings </a>" +
+        "<a class='qwe' href='/Expense-tracker/modern/admin/settings/general-setting/'> General Settings </a>" +
         "<img class='helpdesk-img aside_dropdown_icon' src='/sharepoint-employee-onboarding/modern/admin/js-modern-admin/down-arrow-svgrepo-com.svg'>" +
         "</a>" +
         "</li>" +
         "<li data-nav-id='./Setting/' title='System Setting' class='sidelist dropdown_romove_dot'>" +
         "<ul class='sidenavSub-topics aside_dropdown_list'>" +
         "<li data-nav-id='../General' title='Theme' class='sidelist'>" +
-        "<a href='/Expense-tracker/modern/admin/settings/#themes'>Theme</a>" +
+        "<a href='/Expense-tracker/modern/admin/settings/general-setting/#themes'>Theme</a>" +
         "</li>" +
         "<li data-nav-id='../General' title='Date format' class='sidelist'>" +
-        "<a href='/Expense-tracker/modern/admin/settings/#Dateformat'> Date format </a>" +
+        "<a href='/Expense-tracker/modern/admin/settings/general-setting/#Dateformat'> Date format </a>" +
         "</li>" +
         "<li data-nav-id='../General' title='Currency' class='sidelist'>" +
-        "<a href='/Expense-tracker/modern/admin/settings/#Currency'> Currency </a>" +
+        "<a href='/Expense-tracker/modern/admin/settings/general-setting/#Currency'> Currency </a>" +
         "</li>" +
         "<li data-nav-id='../General' title='Currency Seperator' class='sidelist'>" +
-        "<a href='/Expense-tracker/modern/admin/settings/#CurrencySeperator'> Currency Seperator </a>" +
+        "<a href='/Expense-tracker/modern/admin/settings/general-setting/#CurrencySeperator'> Currency Seperator </a>" +
         "</li>" +
         "<li data-nav-id='../General' title='Language' class='sidelist'>" +
-        "<a href='/Expense-tracker/modern/admin/settings/#Language'> Language </a>" +
+        "<a href='/Expense-tracker/modern/admin/settings/general-setting/#Language'> Language </a>" +
         "</li>" +
         "<li data-nav-id='../General' title='Custom Column' class='sidelist'>" +
-        "<a href='/Expense-tracker/modern/admin/settings/#CustomColumn'> Custom Column </a>" +
+        "<a href='/Expense-tracker/modern/admin/settings/general-setting/#CustomColumn'> Custom Column </a>" +
         "</li>" +
         "<li data-nav-id='../General' title='Expense Future Days' class='sidelist'>" +
-        "<a href='/Expense-tracker/modern/admin/settings/#ExpenseFutureDays'> Expense Future Days </a>" +
+        "<a href='/Expense-tracker/modern/admin/settings/general-setting/#ExpenseFutureDays'> Expense Future Days </a>" +
         "</li>" +
         "<li data-nav-id='../General' title='Display Project Name' class='sidelist'>" +
-        "<a href='/Expense-tracker/modern/admin/settings/#DisplayProjectName'>Display Project Name</a>" +
+        "<a href='/Expense-tracker/modern/admin/settings/general-setting/#DisplayProjectName'>Display Project Name</a>" +
         "</li>" +
         "<li data-nav-id='../General' title='Expense Tracking ID' class='sidelist'>" +
-        "<a href='/Expense-tracker/modern/admin/settings/#ExpenseTrackingID'>Expense Tracking ID</a>" +
+        "<a href='/Expense-tracker/modern/admin/settings/general-setting/#ExpenseTrackingID'>Expense Tracking ID</a>" +
         "</li>" +
         "<li data-nav-id='../General' title='Sharepoint Page Configuration:' class='sidelist'>" +
-        "<a href='/Expense-tracker/modern/admin/settings/#SharepointPageConfiguration'> Sharepoint Page Configuration </a>" +
+        "<a href='/Expense-tracker/modern/admin/settings/general-setting/#SharepointPageConfiguration'> Sharepoint Page Configuration </a>" +
         "</li>" +
 
         "</ul>" +
         "<li data-nav-id='../General' title='Sharepoint Page Configuration:' class='sidelist'>" +
-        "<a class='qwe' href='/Expense-tracker/modern/admin/settings/#UserPermissions'>  User Roles and Permissions</a>" +
+        "<a class='qwe' href='/Expense-tracker/modern/admin/settings/user-role/'>  User Roles and Permissions</a>" +
         "</li>" +
         "</li>" +
         "<li data-nav-id='../General' title='Sharepoint Page Configuration:' class='sidelist'>" +
-        "<a class='qwe' href='/Expense-tracker/modern/admin/settings/#Notification'>Notifications</a>" +
+        "<a class='qwe' href='/Expense-tracker/modern/admin/settings/notification/'>Notifications</a>" +
         "</li>" +
         "</li>" +
 
 
+"<li data-nav-id='../General' title='Integration' class='dropdownicon sidelist'>" +
+        "<a class='qwe' href='/Expense-tracker/modern/admin/settings/integration/'>Integration</a>" +
+         " <img src='/sharepoint-employee-onboarding/modern/admin/js-modern-admin/down-arrow-svgrepo-com.svg'  class='imageIconmodernuser aside_dropdown_icon'>" +
+     "<li data-nav-id='./Settings/'  class='sidelist dropdown_romove_dot'>" +
 
+        "<ul class='sidenavSub-topics aside_dropdown_list active'>" +
+ "<li data-nav-id='../General' title='Zoho Books Integration' class='sidelist'>" +
+        "<a class='qwe' href='/Expense-tracker/modern/admin/settings/integration/#ZohoBooks'>   Zoho Books Integration</a>" +
+        "</li>" +
+         "<li data-nav-id='../General' title='AI API Integration' class='sidelist'>" +
+        "<a class='qwe' href='/Expense-tracker/modern/admin/settings/integration/#_aiapiintegration'>AI API Integration</a>" +
+        "</li>" +
+        "</ul>"+
+        "</li>"+
+        "</li>" +
 
 
 
 
         "<li data-nav-id='../user' title=Feature class='dropdownicon sidelist'>" +
-        "<a class='qwe' href='/Expense-tracker/modern/admin/settings/#Feature'>Features</a>" +
+        "<a class='qwe' href='/Expense-tracker/modern/admin/settings/feature/'>Features</a>" +
         "<img class='helpdesk-img aside_dropdown_icon' src='/sharepoint-employee-onboarding/modern/admin/js-modern-admin/down-arrow-svgrepo-com.svg'>" +
         "</a>" +
         "</li>" +
         "<li data-nav-id='./Setting/' title='System Setting' class='sidelist dropdown_romove_dot'>" +
         "<ul class='sidenavSub-topics aside_dropdown_list'>" +
         "<li data-nav-id='../General' title='Custom Fields' class='sidelist'>" +
-        "<a href='/Expense-tracker/modern/admin/settings/#Custom-Fields'> Custom Fields</a>" +
+        "<a href='/Expense-tracker/modern/admin/settings/feature/#Custom-Fields'> Custom Fields</a>" +
         "</li>" +
         "<li data-nav-id='../General' title='Cost Center' class='sidelist'>" +
-        "<a href='/Expense-tracker/modern/admin/settings/#Cost-Center'>Cost Center</a>" +
+        "<a href='/Expense-tracker/modern/admin/settings/feature/#Cost-Center'>Cost Center</a>" +
         "</li>" +
         "<li data-nav-id='../General' title='Departments' class='sidelist'>" +
-        "<a href='/Expense-tracker/modern/admin/settings/#Department'>Departments</a>" +
+        "<a href='/Expense-tracker/modern/admin/settings/feature/#Department'>Departments</a>" +
         "</li>" +
         "<li data-nav-id='../General' title='Job Title' class='sidelist'>" +
-        "<a href='/Expense-tracker/modern/admin/settings/#Job-title'>Job Title</a>" +
+        "<a href='/Expense-tracker/modern/admin/settings/feature/#Job-title'>Job Title</a>" +
         "</li>" +
         "<li data-nav-id='../General' title='Location' class='sidelist'>" +
-        "<a href='/Expense-tracker/modern/admin/settings/#Location'>Location</a>" +
+        "<a href='/Expense-tracker/modern/admin/settings/feature/#Location'>Location</a>" +
         "</li>" +
         "<li data-nav-id='../General' title='Employee Grades' class='sidelist'>" +
-        "<a href='/Expense-tracker/modern/admin/settings/#Employee-Grades'>Employee Grades</a>" +
+        "<a href='/Expense-tracker/modern/admin/settings/feature/#Employee-Grades'>Employee Grades</a>" +
         "</li>" +
         "<li data-nav-id='../General' title='Expense Type Limits' class='sidelist'>" +
-        "<a href='/Expense-tracker/modern/admin/settings/#Expense-Type-Limits'>Expense Type Limits</a>" +
+        "<a href='/Expense-tracker/modern/admin/settings/feature/#Expense-Type-Limits'>Expense Type Limits</a>" +
         "</li>" +
         "<li data-nav-id='../General' title='Approver Budget Limit' class='sidelist'>" +
-        "<a href='/Expense-tracker/modern/admin/settings/#Approver-Budget-Limit'>Approver Budget Limit</a>" +
+        "<a href='/Expense-tracker/modern/admin/settings/feature/#Approver-Budget-Limit'>Approver Budget Limit</a>" +
         "</li>" +
         "<li data-nav-id='../General' title='Mileage' class='sidelist'>" +
-        "<a href='/Expense-tracker/modern/admin/settings/#Mileage'>Mileage</a>" +
+        "<a href='/Expense-tracker/modern/admin/settings/feature/#Mileage'>Mileage</a>" +
         "</li>" +
         "<li data-nav-id='../General' title='Delete Expense' class='sidelist'>" +
-        "<a href='/Expense-tracker/modern/admin/settings/#Delete-Expense'>Delete Expense</a>" +
+        "<a href='/Expense-tracker/modern/admin/settings/feature/#Delete-Expense'>Delete Expense</a>" +
         "</li>" +
 
         "</ul>" +
@@ -168,7 +182,7 @@ $(document).ready(function () {
         "<a href='/Expense-tracker/modern/admin/help/'>" +
         "Help" +
         "</a>" +
-        "<img src='../../down-arrow-svgrepo-com.svg' class='imageIconadvanced aside_dropdown_icon' '>" +
+        "<img src='/sharepoint-employee-onboarding/modern/admin/js-modern-admin/down-arrow-svgrepo-com.svg' class='imageIconadvanced aside_dropdown_icon' '>" +
         "</li>" +
 
         "<li data-nav-id='../advanced/' title=Help class='sidelist dropdown_romove_dot'>" +
