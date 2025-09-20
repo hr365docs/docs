1b02b835dbb39806ef154affcef43967aad8ac6e
Vidya200230 <poojarividya690@gmail.com>
2025-09-20 11:21:39 +0530

    hr365

--- js/idea-user.js
+++ js/idea-user.js
@@ -4,23 +4,23 @@ $(document).ready(function () {
     $("#ideauser").append(
         // ✅ Introduction
         "<li class='sidelist' data-nav-id='./Introduction/' title='Introduction'>" +
-            "<a href='/Idea-Box-365/modern/user/introduction/'>Introduction</a>" +
+            "<a href='/Idea-365/modern/user/introduction/'>Introduction</a>" +
         "</li>" +
 
         // ✅ Home Page
         "<li class='sidelist' data-nav-id='./Home/' title='Home Page'>" +
-            "<a href='/Idea-Box-365/modern/user/homepage/'>Home Page</a>" +
+            "<a href='/Idea-365/modern/user/homepage/'>Home Page</a>" +
         "</li>" +
 
         // ✅ Help
         "<li class='dropdownicon sidelist' title='Help'>" +
-            "<a href='/Idea-Box-365/modern/user/help/'>Help</a>" +
+            "<a href='/Idea-365/modern/user/help/'>Help</a>" +
               "<img class='helpdesk-img aside_dropdown_icon' src='/sharepoint-employee-onboarding/modern/admin/js-modern-admin/down-arrow-svgrepo-com.svg'>" +
         "</li>" +
         "<li class='sidelist dropdown_romove_dot'>" +
             "<ul class='sidenavSub-topics aside_dropdown_list'>" +
                 "<li class='sidelist' title='Product Activation'>" +
-                    "<a href='/Idea-Box-365/modern/user/help/#product'>Product Activation</a>" +
+                    "<a href='/Idea-365/modern/user/help/#product'>Product Activation</a>" +
                 "</li>" +
             "</ul>" +
         "</li>"
