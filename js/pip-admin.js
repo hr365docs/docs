50064baec35ae67f27d1da766fb3b3dbb62b2cff
Vidya200230 <poojarividya690@gmail.com>
2025-10-08 15:31:07 +0530

    PIP-365

---
+++ js/pip-admin.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+    $("#PIPadmin").html("");
+
+    $("#PIPadmin").append(
+        // ✅ Introduction
+        "<li class='sidelist' data-nav-id='./Settings/' title='Introduction'>" +
+            "<a href='/PIP-365/modern/admin/introduction/'>Introduction</a>" +
+        "</li>" +
+
+        // ✅ Home Page
+        "<li class='sidelist' data-nav-id='./Settings/' title='Home Page'>" +
+            "<a href='/PIP-365/modern/admin/homepage/'>Home Page</a>" +
+        "</li>" +
+
+        // ✅ Settings
+        "<li class='dropdownicon sidelist' title='Settings'>" +
+            "<a href='/PIP-365/modern/admin/settings/'>Settings</a>" +
+              "<img class='helpdesk-img aside_dropdown_icon' src='/sharepoint-employee-onboarding/modern/admin/js-modern-admin/down-arrow-svgrepo-com.svg'>" +
+        "</li>" +
+        "<li class='sidelist dropdown_romove_dot'>" +
+            "<ul class='sidenavSub-topics aside_dropdown_list'>" +
+                "<li class='sidelist' title='Theme'>" +
+                    "<a href='/PIP-365/modern/admin/settings/#_theme_option'>Theme</a>" +
+                "</li>" +
+                "<li class='sidelist' title='Users'>" +
+                    "<a href='/PIP-365/modern/admin/settings/#_users'>Users</a>" +
+                "</li>" +
+                "<li class='sidelist' title='Hide SharePoint Settings'>" +
+                    "<a href='/PIP-365/modern/admin/settings/#_hide_sharepoint_settings'>Hide SharePoint Settings</a>" +
+                "</li>" +
+            "</ul>" +
+        "</li>" +
+
+        // ✅ Help
+        "<li class='dropdownicon sidelist' title='Help'>" +
+            "<a href='/PIP-365/modern/admin/help/'>Help</a>" +
+              "<img class='helpdesk-img aside_dropdown_icon' src='/sharepoint-employee-onboarding/modern/admin/js-modern-admin/down-arrow-svgrepo-com.svg'>" +
+        "</li>" +
+        "<li class='sidelist dropdown_romove_dot'>" +
+            "<ul class='sidenavSub-topics aside_dropdown_list'>" +
+                "<li class='sidelist' title='Product Activation'>" +
+                    "<a href='/PIP-365/modern/admin/help/#product'>Product Activation</a>" +
+                "</li>" +
+            "</ul>" +
+        "</li>"
+    );
+});
