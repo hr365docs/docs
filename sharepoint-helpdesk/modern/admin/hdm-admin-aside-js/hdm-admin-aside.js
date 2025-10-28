e68a9fc8e83903e41e5679183f5b5d426f563045
Vidya200230 <poojarividya690@gmail.com>
2025-10-28 17:22:41 +0530

    kb.hr365

--- sharepoint-helpdesk/modern/admin/hdm-admin-aside-js/hdm-admin-aside.js
+++ sharepoint-helpdesk/modern/admin/hdm-admin-aside-js/hdm-admin-aside.js
@@ -400,11 +400,18 @@ $(document).ready(function () {
         "<ul class='sidenavSub-topics aside_dropdown_list'>" +
         "<li data-nav-id='./Assign-Assets/ ' title='Canned Responses' class='sidelist '>" +
         "<a href='/sharepoint-helpdesk/modern/admin/settings/features/#canned-responses'>Canned Responses</a>" +
+
+         "<li data-nav-id='./Add Assets/ ' title='Column Indexing' class='sidelist '>" +
+        "<ul class='sidenavSub-topics '>" +
+        "</ul>" +
+        "<a href='/sharepoint-helpdesk/modern/admin/settings/features/#Column_Indexing'>Column Indexing</a>" +
+        "</li>" +
         "<li data-nav-id='./Add Assets/ ' title='Customer Satisfaction' class='sidelist '>" +
         "<ul class='sidenavSub-topics '>" +
         "</ul>" +
         "<a href='/sharepoint-helpdesk/modern/admin/settings/features/#customer-satisfaction'>Customer Satisfaction </a>" +
         "</li>" +
+
         "<li data-nav-id='./Add Assets/ ' title='CustomvColumns'class='sidelist '>" +
         "<ul class='sidenavSub-topics '>" +
         "</ul>" +
