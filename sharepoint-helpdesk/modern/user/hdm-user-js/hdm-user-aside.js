33c5168a10f7aa34b53013d5fad2ccf150807399
Vidya200230 <poojarividya690@gmail.com>
2025-11-24 10:37:45 +0530

    kb.hr365

--- sharepoint-helpdesk/modern/user/hdm-user-js/hdm-user-aside.js
+++ sharepoint-helpdesk/modern/user/hdm-user-js/hdm-user-aside.js
@@ -4,10 +4,31 @@ $(document).ready(function() {
     "<li data-nav-id='./Introduction/' title=Introduction class='sidelist'>"+
     "<a href='/sharepoint-helpdesk/modern/user/introduction/'>Introduction</a>"+
     "</li>"+
-    
-    "<li data-nav-id='./Home-Page/' title='Home Page'  class='sidelist'>"+
-    "<a href='/sharepoint-helpdesk/modern/user/home-page/'>Home Page</a>"+
+
+
+
+     "<li class='dropdownicon sidelist'  title=Home Page>" +
+        
+         "<a href='/sharepoint-helpdesk/modern/user/home-page/'>Home Page</a>"+   
+          "<img   class='helpdesk-img aside_dropdown_icon' src='/online-asset-management-application/modern/user/amm-user-assets/down-arrow-svgrepo-com.svg'>"+
+     "</li>"+
+       "<li data-nav-id='./Settings/'   class='sidelist dropdown_romove_dot'>"+
+     "<ul class='sidenavSub-topics aside_dropdown_list'>"+
+
+      "<li data-nav-id='../getting-started/' title='My Requests' class='sidelist'>"+
+    "<a href='/sharepoint-helpdesk/modern/user/home-page/#My-Requests'>My Requests</a>"+
     "</li>"+
+     "<li data-nav-id='../getting-started/' title='My Closed' class='sidelist'>"+
+    "<a href='/sharepoint-helpdesk/modern/user/home-page/#My-Closed'>My Closed</a>"+
+    "</li>"+
+     "<li data-nav-id='../getting-started/' title='Knowledge Base' class='sidelist'>"+
+    "<a href='/sharepoint-helpdesk/modern/user/home-page/#Knowledgebase'>Knowledge Base</a>"+
+    "</li>"+
+
+     "</ul>"+
+     "</li>"+
+    
+ 
 
     "<li data-nav-id='../getting-started/' title='Theme' class='sidelist'>"+
     "<a href='/sharepoint-helpdesk/modern/user/theme/'>Theme</a>"+
